92e804bb0740875fc09abb42af9af51128572c30
sabrek15 <reddy.ksb007@gmail.com>
2025-07-02 15:49:20 +0530

    added unit test cases to test getapikey function

---
+++ internal/auth/auth_test.go
@@ -0,0 +1,68 @@
+package auth
+
+import (
+	"errors"
+	"net/http"
+	"testing"
+)
+
+
+func TestGetAPIKey(t *testing.T) {
+	testCases := []struct {
+		name		string
+		header		http.Header
+		expectedKey	string
+		expectedErr	error
+	} {
+		{
+			name: "vaild_header",
+			header:	http.Header{"Authorization": []string{"ApiKey my-secret-api-key"}},
+			expectedKey: "my-secret-api-key",
+			expectedErr: nil,
+		},
+
+		{
+			name: "missing_header",
+			header: http.Header{},
+			expectedKey: "",
+			expectedErr: ErrNoAuthHeaderIncluded,
+		},
+
+		{
+			name: "malformed_header_no_key",
+			header: http.Header{"Authorization": []string{"Apikey"}},
+			expectedKey: "",
+			expectedErr: errors.New("malformed authorization header"),
+		},
+
+		{
+			name: "malformed_header_wrong_scheme",
+			header: http.Header{"Authorization": []string{"Bearer my-secret-api-key"}},
+			expectedKey: "",
+			expectedErr: errors.New("malformed authorization header"),
+		},
+
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func (t *testing.T) {
+			apikey, err := GetAPIKey(tc.header)
+			
+			if apikey != tc.expectedKey {
+				t.Errorf("expected key '%s', but got '%s'", tc.expectedKey, apikey)
+			}
+			
+			if tc.expectedErr == nil && err != nil {
+				t.Errorf("expected no error, but got '%v'", err)
+			}
+
+			if tc.expectedErr != nil && err == nil {
+				t.Errorf("expected error is '%v', but got nil", tc.expectedErr)
+			}
+
+			if tc.expectedErr != nil && err != nil && tc.expectedErr.Error() != err.Error() {
+				t.Errorf("expected error '%v', but got '%v'", tc.expectedErr, err)
+			}
+		})
+	}
+}
